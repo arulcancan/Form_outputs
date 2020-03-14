86ca6849f6b2b32a200ebe07c132105ca5dc5414
arulcancan <arulcancan@gmail.com>
2020-03-14 12:28:16 +0530

    Update index.blade.php

--- N_Out/resources/views/index.blade.php
+++ N_Out/resources/views/index.blade.php
@@ -10,30 +10,30 @@
         <h5 class="section-heading mb-10" align="right">
             
 
-<table table border="10" cellpadding="10" width="100%">
+        <table table border="10" cellpadding="10" width="100%">
   <tr>
     <td colspan="3" align="text-center"><marquee> <h1><b>Click to view The Nomination  Reoprts </b>  </h1></marquee></td>
   </tr>
 
         <tr> 
-        <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'pe9';">Form PE9</button></td>
-         <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'pe10';">Form PE10</button></td>
-        <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'pe10x1';">Form PE10 (1)</button></td>
+        <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'pe9';">Form PE-9</button></td>
+         <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'pe10';">Form PE-10</button></td>
+        <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'pe10x1';">Form PE10 (1)</button></td>
         </tr>
   <tr> 
-        <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'pe11';">Form PE11)</button></td>
-         <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'pe30x1x2;">Form PE30(1)(2)</button></td>
-        <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'index';">Blank</button></td>
+        <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'pe11';">Form PE11)</button></td>
+         <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'pe30x1x2;">F-PE30(1)(2)</button></td>
+        <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'index';">Blank</button></td>
         </tr>
           <tr> 
-        <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'index';">Blank</button></td>
-         <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'index';">Blank</button></td>
-        <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'index';">Blank</button></td>
+        <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'index';">Blank</button></td>
+         <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'index';">Blank</button></td>
+        <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'index';">Blank</button></td>
         </tr>
         <tr> 
-        <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'index';">Blank</button></td>
-         <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'index';">Blank</button></td>
-        <td align="center"> <button class="btn btn-primary" type="button" onclick="window.location.href = 'index';">Blank</button></td>
+        <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'index';">Blank</button></td>
+         <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'index';">Blank</button></td>
+        <td align="center"> <button class="btn btn-dark" type="button" onclick="window.location.href = 'index';">Blank</button></td>
         </tr>
 </table>
          
